32733cabed90007662f6aefb6cce9e17493fccf8
ひろろ <hrsi.ad@gmail.com>
2021-02-17 11:55:14 +0900

    Initial Commit

---
+++ task5/task5App.swift
@@ -0,0 +1,17 @@
+//
+//  task5App.swift
+//  task5
+//
+//  Created by Hiroshi.Nakai on 2021/02/17.
+//
+
+import SwiftUI
+
+@main
+struct task5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
