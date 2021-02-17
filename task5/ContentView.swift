40e481409b408e64c8c7da16ca6c416aa6711c72
ひろろ <hrsi.ad@gmail.com>
2021-02-18 07:37:42 +0900

    あきおさんFB

--- task5/ContentView.swift
+++ task5/ContentView.swift
@@ -7,11 +7,15 @@
 
 import SwiftUI
 
+struct AlertDetail: Identifiable {
+    let id = UUID()
+    let message: String
+}
+
 struct ContentView: View {
     @State private var textArray = Array(repeating: "", count: 2)
-    @State private var isAlert = false
-    @State private var errorMessage = ""
     @State private var result: Float?
+    @State private var errorAlert: AlertDetail?
 
     var body: some View {
         VStack {
@@ -24,28 +28,28 @@ struct ContentView: View {
             HStack {
                 Button("計算") {
                     guard let num1 = Int(textArray[0]) else {
-                        isAlert = true
-                        errorMessage = "割られる数を入力してください。"
+                        errorAlert = .init(message: "割られる数を入力してください。")
                         return
                     }
 
                     guard let num2 = Int(textArray[1]) else {
-                        isAlert = true
-                        errorMessage = "割る数を入力してください。"
+                        errorAlert = .init(message: "割る数を入力してください。")
                         return
                     }
 
                     if num2 == 0 {
-                        isAlert = true
-                        errorMessage = "割る数には0を入力しないでください。"
+                        errorAlert = .init(message: "割る数には0を入力しないでください。")
                         return
                     }
+                    // errorAlert = .init(message: "OK")
 
                     result = Float(num1) / Float(num2)
 
-                }.alert(isPresented: $isAlert) {
+                    //                }.alert(isPresented: $isAlert) {
+                }.alert(item: $errorAlert) { error in
+                    // $errorAlertがnil以外でアラート表示
                     Alert(title: Text("課題5"),
-                          message: Text(errorMessage))
+                          message: Text(error.message))
                 }
             }.padding()
 
@@ -64,6 +68,7 @@ struct ContentView: View {
 
         return formatter.string(from: NSNumber(value: value)) ?? ""
     }
+
 }
 
 struct InputView: View {
@@ -77,6 +82,7 @@ struct InputView: View {
             )
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
